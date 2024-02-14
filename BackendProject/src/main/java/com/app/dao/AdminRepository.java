f2160996ddb3b2fd9f04d05ec19a25013114aa82
Gaurav <gauravsuryawanshi191@gmail.com>
2024-02-14 20:10:21 +0530

    backend commit3.1

---
+++ BackendProject/src/main/java/com/app/dao/AdminRepository.java
@@ -0,0 +1,24 @@
+package com.app.dao;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.app.pojos.Admin;
+
+public interface AdminRepository extends JpaRepository<Admin,Long> {
+	
+	Optional<Admin> findByFirstNameAndLastName(String firstName, String lastName);
+	
+	Optional<Admin> findByEmailAndPassword(String email, String pwd);
+	
+	Optional<Admin> findByEmail(String email);
+
+	@Modifying
+	@Query("Update Admin a set a.password=:p where a.email=:e")
+	Integer updatePassword(@Param("p") String pass, @Param("e") String em);	
+		
+}
