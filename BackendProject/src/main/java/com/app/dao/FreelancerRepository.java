2c61db1dec09e92d3fe1db6fa689617e6e2b03b3
USERKRUPA <kthumar17@gmail.com>
2024-02-14 20:17:21 +0530

    backend commit3.2

---
+++ BackendProject/src/main/java/com/app/dao/FreelancerRepository.java
@@ -0,0 +1,28 @@
+package com.app.dao;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.app.pojos.Freelancer;
+
+public interface FreelancerRepository extends JpaRepository<Freelancer, Long> {
+
+	Optional<Freelancer> findByEmailAndPassword(String email, String pwd);
+
+	Optional<Freelancer> findByFirstName(String firstName);
+
+	Optional<Freelancer> findByEmail(String em);
+
+	@Modifying
+	@Query("Update Freelancer f set f.password=:p where f.email=:e")
+	Integer updatePassword(@Param("p") String pass, @Param("e") String em);
+
+	@Modifying
+	@Query("Update Freelancer f set f.qualification=:q, f.university=:u, f.passoutYear=:p, f.graduationMarks=:g where f.email=:e")
+	Integer updateFreelancerQualification(@Param("q") String qual, @Param("u") String u,@Param("g") Integer g, @Param("p") Integer p,@Param("e") String em);
+
+}
